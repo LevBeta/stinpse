a42330e4406772487838a9d5ebbfd6f74d84bdab
Beta <105949605+LevBeta@users.noreply.github.com>
2025-02-20 16:33:53 +0000

    Chore: add black_box into bench

--- benches/parser.rs
+++ benches/parser.rs
@@ -1,10 +1,11 @@
 use stinpse::Parser;
+use std::hint::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 const BENCH_INPUT: &str = "ls -l -a -h -b -c -d 'ls -l' \"ls -l\"";
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("parser", |b| b.iter(|| Parser::parse(BENCH_INPUT)));
+    c.bench_function("parser", |b| b.iter(|| black_box(Parser::parse(BENCH_INPUT))));
 }
 
 criterion_group!(benches, criterion_benchmark);
